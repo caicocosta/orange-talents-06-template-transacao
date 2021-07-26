37d8614a5fe592ff5c706111df94403ba7595a6f
Caico Costa <sales.c.costa@gmail.com>
2021-07-26 09:27:27 -0300

    Setup do projeto

---
+++ transacao/src/test/java/br/com/zupacademy/caico/transacao/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.caico.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
