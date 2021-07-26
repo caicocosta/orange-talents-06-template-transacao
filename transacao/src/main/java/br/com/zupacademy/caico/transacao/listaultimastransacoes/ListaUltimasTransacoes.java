9724ad966be643aaf0293061144761fb6d63ed38
Caico Costa <sales.c.costa@gmail.com>
2021-07-26 14:30:19 -0300

    Implementando sistema de autenticação utilizando o Keycloak

--- transacao/src/main/java/br/com/zupacademy/caico/transacao/listaultimastransacoes/ListaUltimasTransacoes.java
+++ transacao/src/main/java/br/com/zupacademy/caico/transacao/listaultimastransacoes/ListaUltimasTransacoes.java
@@ -1,6 +1,5 @@
 package br.com.zupacademy.caico.transacao.listaultimastransacoes;
 
-import br.com.zupacademy.caico.transacao.buscatransacoes.TransacoesRetorno;
 import br.com.zupacademy.caico.transacao.exceptionhandler.ApiErroException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,8 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.stream.Collectors;
 
 @RestController
 public class ListaUltimasTransacoes {
