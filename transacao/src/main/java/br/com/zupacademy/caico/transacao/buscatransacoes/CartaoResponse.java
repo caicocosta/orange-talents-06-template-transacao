e487b5d17a645b686b5abf86798a0a99f7d76343
Caico Costa <sales.c.costa@gmail.com>
2021-07-26 11:37:04 -0300

    Configuração do cliente de transações.

---
+++ transacao/src/main/java/br/com/zupacademy/caico/transacao/buscatransacoes/CartaoResponse.java
@@ -0,0 +1,14 @@
+package br.com.zupacademy.caico.transacao.buscatransacoes;
+
+public class CartaoResponse {
+    private String id;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
